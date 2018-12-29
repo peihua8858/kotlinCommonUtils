23a8ef084879dcb0f4ca7a71973914be8c5b9684
tanping <tanping@globalegrow.com>
2018-12-29 11:27:26 +0800

    新添加String utils

--- app/src/main/java/fz/gb/com/commutils/MainActivity.java
+++ app/src/main/java/fz/gb/com/commutils/MainActivity.java
@@ -3,6 +3,8 @@ package fz.gb.com.commutils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import fz.gb.commutil.text.StringUtil;
+
 /**
  * 测试验证activity
  */
@@ -12,5 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+         boolean b =  StringUtil.isEmpty("");
+
+         assert (b == true);
     }
 }
