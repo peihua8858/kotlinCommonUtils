2e783d2bfd4a07ec342c7b318e08c68fbf8e2fb1
tanping <tanping@globalegrow.com>
2018-12-29 11:15:41 +0800

    服装工具包项公共项目

---
+++ app/src/main/java/fz/gb/com/commutils/MainActivity.java
@@ -0,0 +1,16 @@
+package fz.gb.com.commutils;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 测试验证activity
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
