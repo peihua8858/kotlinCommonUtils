f1581fbb0b6305660ef803f4649a2b24d54ddc1f
dingpeihua <dingpeihua@globalegrow.com>
2021-08-26 08:59:59 +0800

    修复bug

--- commutil/src/main/java/com/fz/common/utils/HtmlUtil.kt
+++ commutil/src/main/java/com/fz/common/utils/HtmlUtil.kt
@@ -1,21 +1,23 @@
 @file:JvmName("HtmlUtil")
+
 package com.fz.common.utils
 
 import android.os.Build
 import android.text.Html
+import android.text.SpannableString
 import android.text.Spanned
 
 /**
- * @author Shyky
- * @version 1.1
- * @email sj1510706@163.com
+ * @return Spanned
  * @date 2016/4/13
  * @since 1.0
  */
 fun String?.fromHtml(): Spanned {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-        Html.fromHtml(this, Html.FROM_HTML_MODE_LEGACY)
-    } else {
-        Html.fromHtml(this)
-    }
+    return this?.let {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            Html.fromHtml(this, Html.FROM_HTML_MODE_LEGACY)
+        } else {
+            Html.fromHtml(this)
+        }
+    } ?: SpannableString("")
 }
