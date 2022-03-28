716e959623e2d37b10be67dd8af3e159a165e703
peihua <379046428@qq.com>
2022-03-28 17:40:35 +0800

    1、增加CodeException

--- commutil/src/main/java/com/fz/common/CodeException.kt
+++ commutil/src/main/java/com/fz/common/CodeException.kt
@@ -2,4 +2,10 @@ package com.fz.common
 
 import java.lang.Exception
 
-class CodeException(msg: String, errorCode: Int) : Exception(msg)
+/**
+ * 带错误码的异常
+ * @author dingpeihua
+ * @date 2022/3/28 17:39
+ * @version 1.0
+ */
+class CodeException(msg: String, val errorCode: Int) : Exception(msg)
