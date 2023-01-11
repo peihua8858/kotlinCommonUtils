76076eb6415d6d5433e3d16ffcc9f681109699fe
peihua <379046428@qq.com>
2023-01-11 16:04:21 +0800

    1、增加recyclerView 滚动到中间的方法

--- commutil/src/main/java/com/fz/common/view/utils/RecyclerViewExt.kt
+++ commutil/src/main/java/com/fz/common/view/utils/RecyclerViewExt.kt
@@ -1,5 +1,6 @@
 package com.fz.common.view.utils
 
+import android.graphics.Rect
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -168,5 +169,148 @@ fun RecyclerView.measureTwoHalf(limitLines: Float, callback: (Int) -> Unit) {
                 callback(realHeight)
             }
         }
+    }
+}
+
+/**
+ * 查找指定类型的item 位置索引
+ * @return 返回查当前类型item索引位置，如果未找到则返回-1
+ * @author dingpeihua
+ * @date 2023/1/11 16:00
+ * @version 1.0
+ */
+fun RecyclerView.Adapter<*>?.findPositionByType(type: Int): Int {
+    if (this == null) {
+        return -1
+    }
+    val itemCount = itemCount
+    for (index in 0 until itemCount) {
+        val itemType = getItemViewType(index)
+        if (itemType == type) {
+            return index
+        }
+    }
+    return -1
+}
+
+/**
+ * 滚动到具有给定偏移量的指定适配器位置
+ * @param position item 位置索引
+ * @param offset 偏移量
+ * @author dingpeihua
+ * @date 2023/1/11 15:53
+ * @version 1.0
+ */
+fun RecyclerView?.scrollToPositionWithOffset(position: Int, offset: Int) {
+    if (this == null) return
+    val manager = layoutManager
+    if (manager is LinearLayoutManager) {
+        manager.scrollToPositionWithOffset(position, offset)
+    } else if (manager is StaggeredGridLayoutManager) {
+        manager.scrollToPositionWithOffset(position, offset)
+    }
+}
+
+/**
+ *滚动指定位置索引到居中位置
+ * @param view 当前item view
+ * @param position 当前item 所在的位置索引
+ * @author dingpeihua
+ * @date 2023/1/11 15:58
+ * @version 1.0
+ */
+fun RecyclerView?.smoothScrollToCenter(view: View, position: Int) {
+    if (this == null) return
+    val rect = Rect()
+    getGlobalVisibleRect(rect)
+    val layoutManager = layoutManager
+    val firstPosition = getStartPosition()
+    val centerView = getChildAt(position - firstPosition)
+    val orientation = when (layoutManager) {
+        is LinearLayoutManager -> {
+            layoutManager.orientation
+        }
+        is StaggeredGridLayoutManager -> {
+            layoutManager.orientation
+        }
+        else -> RecyclerView.VERTICAL
+    }
+    //将点击的position转换为当前屏幕上可见的item的位置以便于计算距离顶部的高度，从而进行移动居中
+    val childAt = getChildAt(position - firstPosition)
+    if (childAt != null) {
+        if (orientation == RecyclerView.VERTICAL) {
+            val reHeight = rect.bottom - rect.top - view.height
+            val top = centerView.top
+            val half = reHeight / 2
+            smoothScrollBy(0, top - half)
+        } else if (orientation == RecyclerView.HORIZONTAL) {
+            val reWidth = rect.right - rect.left - view.width
+            val left = centerView.left
+            val half = reWidth / 2
+            smoothScrollBy(left - half, 0)
+        }
+    }
+}
+
+/**
+ * 滚动指定位置索引到居中位置
+ * @param position 当前item 所在的位置索引
+ * @author dingpeihua
+ * @date 2023/1/11 15:55
+ * @version 1.0
+ */
+fun RecyclerView?.smoothScrollToCenter(position: Int) {
+    if (this == null) return
+    val curView = getChildAt(position)
+    smoothScrollToCenter(curView, position)
+}
+
+/**
+ * 滚动指定位置索引到居中位置
+ * @param position 当前item 所在的位置索引
+ * @author dingpeihua
+ * @date 2023/1/11 15:55
+ * @version 1.0
+ */
+fun RecyclerView?.scrollToCenter(position: Int) {
+    if (this == null) return
+    val curView = getChildAt(position)
+    scrollToCenter(curView, position)
+}
+
+/**
+ * 滚动指定位置索引到居中位置
+ * @param view 当前item view
+ * @param position 当前item 所在的位置索引
+ * @author dingpeihua
+ * @date 2023/1/11 15:55
+ * @version 1.0
+ */
+fun RecyclerView?.scrollToCenter(view: View, position: Int) {
+    if (this == null) return
+    val rect = Rect()
+    getGlobalVisibleRect(rect)
+    val layoutManager = layoutManager
+    val firstPosition = getStartPosition()
+    val centerView = getChildAt(position - firstPosition)
+    val orientation = when (layoutManager) {
+        is LinearLayoutManager -> {
+            layoutManager.orientation
+        }
+        is StaggeredGridLayoutManager -> {
+            layoutManager.orientation
+        }
+        else -> RecyclerView.VERTICAL
+    }
+    if (orientation == RecyclerView.VERTICAL) {
+        val reHeight = rect.bottom - rect.top - view.height
+        val top = centerView.top
+        val half = reHeight / 2
+        scrollBy(0, top - half)
+    } else if (orientation == RecyclerView.HORIZONTAL) {
+        val reWidth = rect.right - rect.left - view.width
+        val left = centerView.left
+        val half = reWidth / 2
+        scrollBy(left - half, 0)
     }
 }
