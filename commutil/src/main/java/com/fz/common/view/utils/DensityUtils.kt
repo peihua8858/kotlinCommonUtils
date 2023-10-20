6ad1419bb40c8ec12fa0b5f9f3c5163bd39c3c3f
peihua <379046428@qq.com>
2023-10-20 16:39:30 +0800

    1、增加像素密度新方法

--- commutil/src/main/java/com/fz/common/view/utils/DensityUtils.kt
+++ commutil/src/main/java/com/fz/common/view/utils/DensityUtils.kt
@@ -7,12 +7,48 @@ import android.app.Activity
 import android.app.Dialog
 import android.content.Context
 import android.content.res.Resources
+import android.util.TypedValue
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.fz.common.ContextInitializer
 import com.fz.common.utils.checkContext
 import com.fz.common.utils.eLog
 import kotlin.math.roundToInt
+import kotlin.math.roundToLong
+
+inline val Int.dp: Int get() = toFloat().dp.roundToInt()
+
+inline val Long.dp: Long get() = toFloat().dp.roundToLong()
+
+inline val Double.dp: Double get() = toFloat().dp.toDouble()
+
+inline val Float.dp: Float
+    get() = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this, Resources.getSystem().displayMetrics)
+inline val Int.sp: Int get() = toFloat().sp.roundToInt()
+
+inline val Long.sp: Long get() = toFloat().sp.roundToLong()
+
+inline val Double.sp: Double get() = toFloat().sp.toDouble()
+inline val Float.sp: Float
+    get() = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, this, Resources.getSystem().displayMetrics)
+
+fun Int.pxToDp(): Int = toFloat().px2dp().roundToInt()
+
+fun Long.pxToDp(): Long = toFloat().px2dp().roundToLong()
+
+fun Double.pxToDp(): Double = toFloat().px2dp().toDouble()
+
+fun Float.px2dp(): Float =
+    (this / Resources.getSystem().displayMetrics.density + 0.5f)
+
+fun Int.px2sp(): Int = toFloat().px2sp().roundToInt()
+
+fun Long.px2sp(): Long = toFloat().px2sp().roundToLong()
+
+fun Double.px2sp(): Double = toFloat().px2sp().toDouble()
+
+fun Float.px2sp(): Float =
+    (this / Resources.getSystem().displayMetrics.scaledDensity + 0.5f)
 
 /**
  *  在[View]、 [Context]、[Activity]、[Fragment]、[Dialog]中可直接使用该方法将dp值转px
